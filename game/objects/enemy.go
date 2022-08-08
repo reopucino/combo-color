b2dc7e113fc1089561c34f81b4b3893b8804a8af
Reza Akbar <reza.rezqya@gmail.com>
2022-08-08 23:20:47 +0700

    checkpoint

--- game/objects/enemy.go
+++ game/objects/enemy.go
@@ -1,3 +1,18 @@
+components {
+  id: "enemy"
+  component: "/game/objects/enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -5,6 +20,24 @@ embedded_components {
   "default_animation: \"enemyFloating_1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory_spell"
+  type: "factory"
+  data: "prototype: \"/game/objects/target_spell.go\"\n"
+  "load_dynamically: false\n"
   ""
   position {
     x: 0.0
--- game/objects/target_spell.go
+++ game/objects/target_spell.go
@@ -1,8 +1,8 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"\"\n"
-  "default_animation: \"\"\n"
+  data: "tile_set: \"/game/ui.atlas\"\n"
+  "default_animation: \"target\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
