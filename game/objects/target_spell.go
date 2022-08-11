54b79c0ad914ff7acf1fc980cd88c9c9d8d8de08
Reza Rezqya Akbar <reza.rezqya@gmail.com>
2022-08-11 18:57:35 +0700

    update

--- game/objects/target_spell.go
+++ game/objects/target_spell.go
@@ -1,3 +1,18 @@
+components {
+  id: "target_spell"
+  component: "/game/objects/target_spell.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
